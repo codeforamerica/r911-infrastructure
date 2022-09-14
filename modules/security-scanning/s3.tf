41d99592c80947b5be6759ef4ce78aa73915d6ad
James Armes (they/them) <jarmes@codeforamerica.org>
2022-09-14 13:06:20 -0400

    Added explicit public access deny blocks for S3 buckets. (#2)

--- modules/security-scanning/s3.tf
+++ modules/security-scanning/s3.tf
@@ -6,6 +6,15 @@ resource "aws_s3_bucket" "config" {
   }
 }
 
+resource "aws_s3_bucket_public_access_block" "config" {
+  bucket = aws_s3_bucket.config.bucket
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
 resource "aws_s3_bucket_acl" "config" {
   bucket = aws_s3_bucket.config.id
   acl    = "private"
